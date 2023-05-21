b2b440b6da07a176ede0d624e1986b68a0690909
Viraj1725 <130982560+Viraj1725@users.noreply.github.com>
2023-05-21 17:28:54 +0530

    Day1

--- Day1/Memoization.java
+++ Day1/Memoization.java
@@ -1,1 +1,38 @@
-//
+import java.util.HashMap;
+import java.util.Map;
+
+//Day 1 (19/05/2023)
+//Pascal Triangle example using memoization
+
+public class Memoization {
+    private static Map<Integer, Map<Integer, Integer>> cache = new HashMap<>(); // Used HashMap(key-value pair)
+
+    public static void calculatePascalTriangle(int totalRows) {
+        for (int i = 0; i < totalRows; i++) {
+            for (int j = 0; j <= i; j++) {
+                int coefficient = calculateCoefficient(i, j);
+                System.out.print(coefficient + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static int calculateCoefficient(int rows, int columns) {
+        if (columns == 0 || columns == rows) {
+            return 1;
+        } else if (cache.containsKey(rows) && cache.get(rows).containsKey(columns)) {
+            return cache.get(rows).get(columns);
+        } else {
+            int coefficient = calculateCoefficient(rows - 1, columns - 1) + calculateCoefficient(rows - 1, columns);
+            cache.putIfAbsent(rows, new HashMap<>());
+            cache.get(rows).put(columns, coefficient);
+            return coefficient;
+        }
+    }
+    
+    public static void main(String[] args) {
+        int totalRows = 5; 
+        calculatePascalTriangle(totalRows);
+    }
+}
+//Used Memoization for first time so took reference of google
