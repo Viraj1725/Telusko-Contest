ec71a160b10d642b7ab3007f0672d050110b6c9a
Viraj1725 <130982560+Viraj1725@users.noreply.github.com>
2023-05-21 17:29:49 +0530

    Day1

--- Day1/Recursion.java
+++ Day1/Recursion.java
@@ -1,1 +1,27 @@
-//
+package com.enterops.viraj;
+
+//Day 1 (19/05/2023)
+// Pascal Triangle example using recursion
+
+public class recursion {
+	
+    public static int pascalTriangle(int rows, int columns) {
+        if (columns == 0 || columns == rows) {
+            return 1;
+        } else {
+            return pascalTriangle(rows - 1, columns - 1) + pascalTriangle(rows - 1, columns);
+        }
+    }
+    
+    public static void main(String[] args) {
+        int rows = 6;
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print(pascalTriangle(i,j));
+            }
+            System.out.println();
+        }
+    }
+
+}
+//Here pascalTriangle method takes the row and column values as parameters and use recursion to calculate value of Pascal Triangle.
