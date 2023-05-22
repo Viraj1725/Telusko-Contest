9166c312c8292b689e42953deaee7f727884d3b1
Viraj1725 <130982560+Viraj1725@users.noreply.github.com>
2023-05-22 21:27:57 +0530

    Day3 commit

--- ProductMgmtSpringBoot/ProductService.java
+++ ProductMgmtSpringBoot/ProductService.java
@@ -1,7 +1,8 @@
 package com.enterops.ebiz.draft.product.TenDayChallenge;
 
 import java.util.List;
-
+import java.util.ArrayList;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,7 +13,6 @@ public class ProductService {
     ProductDB db;	
 
     //ProductDB db = new ProductDB();
-
 //    public void addProduct(Product p){
 //       // products.add(p);
 //    	db.save(p);
@@ -26,13 +26,39 @@ public class ProductService {
 //                return p;
 //        }
 //        return null;
-//    }
+//    }   
+    List<Product> products = new ArrayList<>();
+    
     public List<Product> getAllProducts(){
         return db.findAll();
     }
     
-    public List<Product> getProducts(String string){
-        return db.findByName(string);
-    }
+    public List<Product> searchProductsByPlace(String place){
+    	String str = place.toLowerCase();
+        List<Product> prods = new ArrayList<>();
+        List<Product> products = db.findAll();
            
-}
+        for(Product p: products){
+        	String searchplace = p.getPlace().toLowerCase();
+        	if(searchplace.contains(str)){
+                prods.add(p);
+            }
+        }
+        return prods;
+    }
+    
+    int year;
+	public List<Product> searchProductsOutOfWarranty(int year){
+		List<Product> prods = new ArrayList<>();
+		List<Product> products = db.findAll();
+
+		for(Product p:products) {
+			Integer searchwarranty = p.getWarranty();
+			this.year = year;
+			if(searchwarranty < year) {
+				prods.add(p);
+		 	}
+		}
+		return prods;
+	}
+ }       
